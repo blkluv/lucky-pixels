0f01f6ae9dcb9c5a6345de9e2941b63a1363daaa
benjiminii <bilguunaiax7@gmail.com>
2023-08-16 18:37:49 +0800

    Debug log added

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -38,6 +38,7 @@ export async function POST(request: Request) {
 
   if (relevantEvents.has(event.type)) {
     try {
+      console.log(`Webhook event ${event.type}`);
       switch (event.type) {
         case "product.created":
         case "product.updated":
